5160f76faae746a26911ac8ac5f3ad56d97dca8c
AvivShlomi <89010861+AvivShlomi@users.noreply.github.com>
2022-06-15 16:31:00 +0300

    Update UITest.java

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -1,8 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class UITest {
 
